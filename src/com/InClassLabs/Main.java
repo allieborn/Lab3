e64f3072838bd219ed4564585b41fc0de74488ae
Alexandra <aborn601722@Alexandras-MacBook-Pro.local>
2017-07-18 11:29:23 -0400

    added addt validation

--- src/com/InClassLabs/Main.java
+++ src/com/InClassLabs/Main.java
@@ -5,26 +5,40 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         System.out.println("Learn your squares and cubes!");
         System.out.println();
 
-        String userInput;
+        String userInput = "y";
+        int userNum;
 
         do {
-            System.out.println("Enter an integer: ");
-            int userNum = scan.nextInt();
-            System.out.println("Number" + "\tSquared" + "\tCubed");
-            for (int i = 1; i <= userNum; i++) {
-                System.out.println(i + "\t" + (i * i) + "\t" + (i * i * i));
+            System.out.println("Enter an integer less than 100: ");
+            userNum = scan.nextInt();
+
+            if ((userNum < 1) || (userNum > 100)) {
+                System.out.println("The integer you entered was too large.");
             }
-            scan.nextLine();
-            System.out.println("Continue? Y/N:");
-            userInput = scan.nextLine();
-            if (userInput.equalsIgnoreCase("n")){
-                System.out.println("Goodbye.");
+            else {
+                squaresAndCubes(userNum);
+                scan.nextLine();
+                System.out.println("Continue? Y/N:");
+                userInput = scan.nextLine();
+                if (userInput.equalsIgnoreCase("n")) {
+                    System.out.println("Goodbye.");
+                }
             }
         } while (userInput.equalsIgnoreCase("y"));
 
     }
+
+
+    public static void squaresAndCubes(int userNum) {
+        System.out.println("Number" + "\tSquared" + "\tCubed");
+        for (int i = 1; i <= userNum; i++) {
+            System.out.println(i + "\t" + (i * i) + "\t" + (i * i * i));
+        }
+    }
+
 }
